67a411030d741438c3772fb3f492461b3499c3e7
Jvco <65981077+zJvco@users.noreply.github.com>
2021-09-24 11:13:54 -0300

    Alterando o campo image na aba Product para um campo de upload de imagem

--- app/admin.py
+++ app/admin.py
@@ -1,16 +1,26 @@
+from flask import redirect, url_for
+from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
-
-from wtforms.fields import FileField
-from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 from . import adm, db
 from .models import User, Product
 
 
 def configure():
+    adm.index_view = IndexAdmin()
+
     adm.add_view(UserAdmin(User, db.session))
     adm.add_view(ProductsAdmin(Product, db.session))
+
+
+class IndexAdmin(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("views.home_page"))
 
 
 class UserAdmin(ModelView):
@@ -21,12 +31,21 @@ class UserAdmin(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.admin
 
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("admin.index"))
+
 
 class ProductsAdmin(ModelView):
     form_excluded_columns = ["created_date", "updated_date", "user_products"]
     column_exclude_list = ["image"]
 
-    column_type_formatters = {"image": FileField(label="Image", validators=[DataRequired()])}
+    def scaffold_form(self):
+        form = super().scaffold_form()
+        form.image = FileField(label="Image", validators=[FileRequired(), FileAllowed(["jpg", "png"], "You can upload only images")])
+        return form
 
     def is_accessible(self):
         return current_user.is_authenticated and current_user.admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("admin.index"))
